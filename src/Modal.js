e8d7072a8f52b31316df856da43b6f406f4419eb
rafaelangical <rsnrafael@outlook.com>
2018-02-13 11:44:23 -0300

    Changes

--- src/Modal.js
+++ src/Modal.js
@@ -2,20 +2,42 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import logo from './logo.jpg';
 
-export class Modal extends Component {
-  
-  state = {
-    position: 5
+
+class Modal extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      position: 0,
+    }
+  }
+
+  next = () => {
+    this.setState({ position: this.state.position + 1 },
+      () => {
+        console.log("next");
+        if (this.state.position > 11) {
+          this.setState({ position: 0 });
+          console.log("igual a length of arrays");
+        }
+      });
   };
 
-  next = (position) => {
-    this.setState({position: position - 1})
+  prev = () => {
+    if (this.state.position > 0) {
+      this.setState({ position: this.state.position - 1 },
+        () => {
+
+          console.log("prev");
+
+        });
+    }
   };
 
-  prev = (position) => {
-    this.setState({position: position + 1})
-  };
-  
+  setImageByTarget = (key) => {
+    this.setState({ position: key.target.value })
+    console.log("Funcionou " + key.target.value)
+  }
+
   render() {
 
     //Não renderiza se show for false
@@ -23,23 +45,22 @@ export class Modal extends Component {
       return null;
     }
 
-    // The modal "window"
     const modalStyle = {
-      minWidth: "50%",
-      minHeight: "50%",
+      minWidth: "100%",
+      minHeight: "100%",
+      position: "fixed",
       margin: "0 auto",
       padding: 30,
       display: "flex",
-      flexFlow: "column wrap"
     };
+
     const modalContent = {
       margin: "0 auto",
       position: "absolute",
+      position: "fixed",
     };
 
-
-    //tests 
-    const images  = [
+    const images = [
       'http://lorempixel.com/400/200',
       'http://lorempixel.com/400/300',
       'http://lorempixel.com/400/400',
@@ -52,56 +73,52 @@ export class Modal extends Component {
       'http://lorempixel.com/400/100',
       'http://lorempixel.com/500/200',
       'http://lorempixel.com/300/300'
-    ] 
+    ];
 
+    const mapImages = images.map((src, index) => {
+      return <img key={index} className="image-modal-principal" alt="images-modal" src={src} />
+    });
 
-    //make map for array of images
-    const mapImages = images.map((src,index) => {
-      return <img key={index} className="image-modal-principal" alt="" src={src} /> 
-    })
+    const filterImage = mapImages.filter((src, index) => index === this.state.position);
 
-    const filterImage = mapImages.filter((src,index) =>  index === this.props.position );  
+    const setImageWindow = filterImage.filter((src, index) => {
+      return <a href="" onlick={() => this.setImageByTarget(this.index)}><img key={index} alt="images-modal" src={src} className="image-modal-principal"/></a>
+    });
 
-    const setImageWindow = filterImage.filter((src,index) => {
-      return <img key={index} alt="" src={src} className="image-modal-principal" onClick={this.}/>
-    })
-
-
-    console.log(this.next)
-    console.log(this.prev)
-    console.log("value:"+" = "+this.position)
 
     return (
-        <div className="modal-back" style={modalStyle} id="novo">
-          <div className="modal-content" style={modalContent}>
-            <a className="close cursor" onClick={this.props.onClose}>
-              &times;
+      <div className="modal-back" style={modalStyle} id="novo">
+        <div className="modal-content" style={modalContent}>
+          <a className="close cursor" onClick={() => this.props.onClose()}>
+            &times;
             </a>
-            <div className="columnOne">
-              <a href="#" className="prev" onClick={this.prev}>
-                &#10094;
+          <div className="columnOne">
+            <a href="#" className="prev" onClick={this.prev}>
+              &#10094;
               </a>
-            </div>
-            <div className="columnTwo">
-              {setImageWindow}
-            </div>
-            <div className="columnThere">
-              <div className="row1-columThere">
-                <a href="#">
-                  <img src={logo} className="image-logo-modal" alt="img-modal"/>
-                </a>
-                <a href="#">fael.s.n</a>
-              </div>
-              <div className="row2-columnThere" />
-              <div className="row3-columnThere" />
-              <div className="row4-columnThere" />
-            </div>
-            <div className="columnFour">
-              <a href="#" className="next" onClick={this.next}>
-                &#10095;
-              </a>
-            </div>
           </div>
+          <div className="columnTwo">
+            {setImageWindow}
+          </div>
+          <div className="columnThere">
+            <div className="row1-columThere">
+              <a href="#">
+                <img src={logo} className="image-logo-modal" alt="img-modal" />
+                <a href="#">fael.s.n</a>
+                <br />
+                <a href="#">fael.s.n</a>
+              </a>
+            </div>
+            <div className="row2-columnThere" />
+            <div className="row3-columnThere" />
+            <div className="row4-columnThere" />
+          </div>
+          <div className="columnFour">
+            <a href="#" className="next" onClick={this.next}>
+              &#10095;
+              </a>
+          </div>
+        </div>
       </div>
     );
   }
@@ -112,4 +129,4 @@ Modal.propTypes = {
   show: PropTypes.bool,
 };
 
-export default Modal;
+export default Modal
